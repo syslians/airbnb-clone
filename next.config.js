5562118fc1e134c8b909851474ddaeaaa8838b1b
syslians <tonynala97.naver.com>
2023-08-06 14:45:36 +0900

    my commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: [
-      "lh3.googleusercontent.com",
+      // "lh3.googleusercontent.com",
       "avatars.githubsercontent.com",
       "res.cloudinary.com"
     ]
-  },
-  experimental: {
-    appDir: true,
   }
 }
 
