031b6da44efae082930b417d40aacab59c1a550a
syslians <tonynala97.naver.com>
2023-11-13 21:44:13 +0900

    s

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      // 다른 외부 이미지 호스팅 도메인이 있다면 여기에 추가하세요.
+    domains: [
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com"
     ],
   },
-  // 다른 설정들...
+  experimental: {
+    appDir: true,
+  },
+
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
